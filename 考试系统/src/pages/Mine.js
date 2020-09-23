1f001991399edc287df624f7b577582242d82a70
daifang <784433957@qq.com>
2020-09-23 09:25:12 +0800

    test

--- 考试系统/src/pages/Mine.js
+++ 考试系统/src/pages/Mine.js
@@ -19,19 +19,16 @@ export default class Mine extends Component {
             }
         }).then(res=>{
             console.log(res);
-            if(res.data.status == 200){
+            if(res.data.status == 40301){
+                alert('登录状态已过期,请重新登录');
+                localStorage.setItem('userId','');
+                window.location.hash = '/';
+            }else{
                 this.setState({
                     username:res.data.stuName?res.data.stuName:'获取中',
                     userSchool:res.data.stuProfessional?res.data.stuProfessional:'...',
                     imgSrc:res.data.imgSrc?res.data.imgSrc:this.state.imgSrc
                 });
-            }else if(res.data.status == 40301){
-                alert('登录状态已过期,请重新登录');
-                localStorage.setItem('userId','');
-                window.location.hash = '/';
-            }else{
-                localStorage.setItem('userId','');
-                window.location.hash = '/';
             }
         })
     }
